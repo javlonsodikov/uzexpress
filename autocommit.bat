318fe2a8949b7640e0426a6437acb5ce27a837d3
Javlon Sodikov <javlonuser@yahoo.com>
2016-05-30 10:25:19 +0500

    autocommit 30.05.2016-10:12:37,71

--- autocommit.bat
+++ autocommit.bat
@@ -1,11 +1,15 @@
 :; echo "Hi, I’m ${SHELL}."; exit $?
 @ECHO OFF
 ECHO I'm %COMSPEC%
-pause
+rem pause
 rem exit
 cd D:\www\yii2_composer\uzexpress
 set _date=%DATE:/=-%
-set _time=%TIME::=-%
+set __time=%TIME::=-%
+set _time=%__time:,=%
+echo %_time:~0,8%
+pause
+exit
 
 set /p dbpassword=<"C:\Program Files (x86)\Ampps\ampps\data\my.conf"
 "C:\Program Files (x86)\Ampps\mysql\bin\mysqldump.exe" -e -uroot -p%dbpassword% aliadvanced > db_dump\%_date%-%_time%.sql
