d27f3296da448f3f21976bcbfb4719cf562e0c0a
Javlon Sodikov <javlonuser@yahoo.com>
2016-05-30 10:35:22 +0500

    autocommit 30.05.2016-10-35-21

--- autocommit.bat
+++ autocommit.bat
@@ -14,7 +14,7 @@ set _time=%TIME::=-%
 set _time=%_time:~0,8%
 set /p dbpassword=<"C:\Program Files (x86)\Ampps\ampps\data\my.conf"
 "C:\Program Files (x86)\Ampps\mysql\bin\mysqldump.exe" -e -uroot -p%dbpassword% aliadvanced > db_dump\%_date%-%_time%.sql
-git pull
+git pull origin master
 git add .
 git commit -m "autocommit %_date%-%_time%"
 git push origin master 
