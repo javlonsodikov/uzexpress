0cf496a700d2d8d7b9ed2e5927721cd78249f0e0
Javlon Sodikov <javlonuser@yahoo.com>
2016-05-30 16:05:04 +0500

    autocommit 30.05.2016-16-05-02

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=uzexpress',
-    'username' => 'uzexpressdb',
+    'username' => 'uzexpress',
     'password' => 'QfUHqyvKsCMHtrvE',
     'charset' => 'utf8', 
 ];
