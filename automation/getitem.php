3b9a263e51937635c59ecf0a1d6879a0f683bf35
Javlon Sodikov <javlonuser@yahoo.com>
2016-05-30 14:05:00 +0500

    autocommit 30.05.2016-14-04-56

--- automation/getitem.php
+++ automation/getitem.php
@@ -1,13 +1,17 @@
 <?php
-$arr = include __DIR__."/../config/db.php";
-$key=$argv[1];
-if ($key=="dbname"){
-	$dsn=$arr["dsn"];
-	$values=explode(";", $dsn);
-	$dbval=array_pop($values);
-	$_dbname=explode("=", $dbval);
-	$dbname=array_pop($_dbname);
-	echo $dbname;
-	exit;
+if (file_exists(__DIR__."/../config/db.php")){
+	$arr = include __DIR__."/../config/db.php";
+	$key=$argv[1];
+	if ($key=="dbname"){
+		$dsn=$arr["dsn"];
+		$values=explode(";", $dsn);
+		$dbval=array_pop($values);
+		$_dbname=explode("=", $dbval);
+		$dbname=array_pop($_dbname);
+		echo $dbname;
+		exit;
+	}
+	echo (isset($arr[$key])?$arr[$key]:"");
 }
-echo (isset($arr[$key])?$arr[$key]:"");
+else 
+	exit(-1);
