4360825a8e14c040726fd4059e110c35eabb42be
Javlon Sodikov <javlonuser@yahoo.com>
2016-05-30 12:22:12 +0500

    autocommit 30.05.2016-12-22-11

--- automation/autocommit.bat
+++ automation/autocommit.bat
@@ -19,13 +19,7 @@ set _time=%_time:~0,8%
 for /f %%i in ('php getitem.php dbname') do set dbname=%%i
 for /f %%i in ('php getitem.php username') do set dbusername=%%i
 for /f %%i in ('php getitem.php password') do set dbpassword=%%i
-echo %dbusername%
-echo %dbpassword%
-echo %dbname%
-
-rem set /p dbpassword=<"C:\Program Files (x86)\Ampps\ampps\data\my.conf"
 "C:\Program Files (x86)\Ampps\mysql\bin\mysqldump.exe" -u%dbusername% -p%dbpassword% %dbname% > db_dump\%_date%-%_time%.sql
-
 git pull origin master
 git add .
 git commit -m "autocommit %_date%-%_time%"
