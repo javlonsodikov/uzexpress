7d8a0c0993a07f981d793716061db5677e711897
Javlon Sodikov <javlonuser@yahoo.com>
2016-05-30 12:11:56 +0500

    autocommit 30.05.2016-12-11-49

--- automation/autocommit.bat
+++ automation/autocommit.bat
@@ -3,17 +3,26 @@
 GOTO :CMDSCRIPT
 ::CMDLITERAL
 echo "Linux shell autocommit"
-exit $?
+
+
+exit 0
+
+
 
 :CMDSCRIPT
-
 echo "windows autocommit"
-cd D:\www\yii2_composer\uzexpress
+set workdir="D:\www\yii2_composer\uzexpress\automation"
+cd %workdir%
 set _date=%DATE:/=-%
 set _time=%TIME::=-%
 set _time=%_time:~0,8%
-set /p dbpassword=<"C:\Program Files (x86)\Ampps\ampps\data\my.conf"
-"C:\Program Files (x86)\Ampps\mysql\bin\mysqldump.exe" -uroot -p%dbpassword% aliadvanced > db_dump\%_date%-%_time%.sql
+pause
+for /f %%i in ('php getitem.php username') do set dbusername=%%i
+pause
+for /f %%i in ('php getitem.php password') do set dbpassword=%%i
+
+rem set /p dbpassword=<"C:\Program Files (x86)\Ampps\ampps\data\my.conf"
+"C:\Program Files (x86)\Ampps\mysql\bin\mysqldump.exe" -u%dbusername% -p%dbpassword% aliadvanced > db_dump\%_date%-%_time%.sql
 git pull origin master
 git add .
 git commit -m "autocommit %_date%-%_time%"
