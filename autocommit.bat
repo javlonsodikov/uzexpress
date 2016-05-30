ca1b55e524cb63348d45170ec2b7c3f5af652e84
Javlon Sodikov <javlonuser@yahoo.com>
2016-05-30 10:12:05 +0500

    autocommit 30.05.2016-10:12:05,72

--- autocommit.bat
+++ autocommit.bat
@@ -1,9 +1,8 @@
 cd D:\www\yii2_composer\uzexpress
 set _date=%DATE:/=-%
+set _time=%TIME:/=-%
 set /p dbpassword=<"C:\Program Files (x86)\Ampps\ampps\data\my.conf"
-"C:\Program Files (x86)\Ampps\mysql\bin\mysqldump.exe" -e -uroot -p%dbpassword% aliadvanced > db_dump\%_date%.sql
-pause
+"C:\Program Files (x86)\Ampps\mysql\bin\mysqldump.exe" -e -uroot -p%dbpassword% aliadvanced > db_dump\%_date%-%_time%.sql
 git add .
-git commit -m "autocommit %_date%"
+git commit -m "autocommit %_date%-%_time%"
 git push origin master 
-pause 
