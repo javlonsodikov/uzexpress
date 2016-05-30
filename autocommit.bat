2d02dc9e35c1b5ab3df7f735b0fffc318ccb3383
Javlon Sodikov <javlonuser@yahoo.com>
2016-05-30 10:43:55 +0500

    autocommit 30.05.2016-10-43-54

--- autocommit.bat
+++ autocommit.bat
@@ -13,7 +13,7 @@ set _date=%DATE:/=-%
 set _time=%TIME::=-%
 set _time=%_time:~0,8%
 set /p dbpassword=<"C:\Program Files (x86)\Ampps\ampps\data\my.conf"
-"C:\Program Files (x86)\Ampps\mysql\bin\mysqldump.exe"  --skip-add-drop-table -e -uroot -p%dbpassword% aliadvanced > db_dump\%_date%-%_time%.sql
+"C:\Program Files (x86)\Ampps\mysql\bin\mysqldump.exe" -uroot -p%dbpassword% aliadvanced > db_dump\%_date%-%_time%.sql
 git pull origin master
 git add .
 git commit -m "autocommit %_date%-%_time%"
