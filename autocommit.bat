cdad42ac38a373408fefaac55bc46cdbbb776119
Javlon Sodikov <javlonuser@yahoo.com>
2016-05-30 10:22:53 +0500

    autocommit 30.05.2016-10-22-53,08

--- autocommit.bat
+++ autocommit.bat
@@ -1,6 +1,12 @@
+:; echo "Hi, I’m ${SHELL}."; exit $?
+@ECHO OFF
+ECHO I'm %COMSPEC%
+pause
+rem exit
 cd D:\www\yii2_composer\uzexpress
 set _date=%DATE:/=-%
-set _time=%TIME:/=-%
+set _time=%TIME::=-%
+
 set /p dbpassword=<"C:\Program Files (x86)\Ampps\ampps\data\my.conf"
 "C:\Program Files (x86)\Ampps\mysql\bin\mysqldump.exe" -e -uroot -p%dbpassword% aliadvanced > db_dump\%_date%-%_time%.sql
 git add .
